01ac7520e243c131a671abc76c72b6dc72c6a05f
Xdoxgg <m.zhuk007@gmail.com>
2025-09-20 16:33:54 +0300

    finish lab

--- tasks.js
+++ tasks.js
@@ -168,3 +168,181 @@ function expandedForm(num) {
 
     return parts.join(' + ');
 }
+//9
+
+function solution(str) {
+    if (str.length % 2 !== 0) {
+        str += '_';
+    }
+    let result = [];
+    for (let i = 0; i < str.length; i += 2) {
+        result.push(str.slice(i, i + 2));
+    }
+    return result;
+
+}
+
+
+//10
+
+function bingo(ticket, win) {
+    let miniWins = 0;
+
+    for (const [str, num] of ticket) {
+        if ([...str].some(char => char.charCodeAt(0) === num)) {
+            miniWins++;
+        }
+    }
+
+    return miniWins >= win ? 'Winner!' : 'Loser!';
+}
+
+//11
+
+function domainName(url) {
+    url = url.replace(/^(https?:\/\/)?(www\.)?/, '');
+    let domain = url.split('/')[0];
+    return domain.split('.')[0];
+}
+
+//12
+
+function longest(arr, n) {
+    const mapped = arr.map((str, index) => ({ str, length: str.length, index }));
+
+    mapped.sort((a, b) => {
+        if (b.length !== a.length) {
+            return b.length - a.length;
+        }
+        return a.index - b.index;
+    });
+
+    return mapped[n - 1].str;
+}
+
+//13
+
+function hexStringToRGB(hexString) {
+    const hex = hexString.replace(/^#/, '').toLowerCase();
+
+    const r = parseInt(hex.substring(0, 2), 16);
+    const g = parseInt(hex.substring(2, 4), 16);
+    const b = parseInt(hex.substring(4, 6), 16);
+
+    return { r, g, b };
+}
+
+//14
+
+
+function isCircleSorted(arr) {
+    let countDrops = 0;
+    const n = arr.length;
+
+    for (let i = 0; i < n; i++) {
+        if (arr[i] > arr[(i + 1) % n]) {
+            countDrops++;
+            if (countDrops > 1) return false;
+        }
+    }
+
+    return true;
+}
+
+//15
+function howManyTimes(time1, time2) {
+    const start = new Date(time1);
+    const end = new Date(time2);
+    let strikes = 0;
+
+    function strikesAt(date) {
+        const minutes = date.getMinutes();
+        const hours24 = date.getHours();
+        if (minutes === 0) {
+            return hours24 % 12 === 0 ? 12 : hours24 % 12;
+        } else if (minutes === 30) {
+            return 1;
+        }
+        return 0;
+    }
+
+    let firstStrike = new Date(start);
+    firstStrike.setSeconds(0, 0);
+    const startMinutes = firstStrike.getMinutes();
+    if (startMinutes % 30 !== 0) {
+        const addMinutes = 30 - (startMinutes % 30);
+        firstStrike.setMinutes(firstStrike.getMinutes() + addMinutes);
+    }
+
+    for (
+        let strikeTime = new Date(firstStrike);
+        strikeTime < end;
+        strikeTime.setMinutes(strikeTime.getMinutes() + 30)
+    ) {
+        const count = strikesAt(strikeTime);
+        if (count === 0) continue;
+
+        const strikeStart = strikeTime.getTime();
+        const strikeEnd = strikeStart + count * 1000;
+
+        const intervalStart = Math.max(strikeStart, start.getTime());
+        const intervalEnd = Math.min(strikeEnd, end.getTime());
+
+        if (intervalEnd > intervalStart) {
+            strikes += Math.floor((intervalEnd - intervalStart) / 1000);
+        }
+    }
+
+    return strikes;
+}
+
+//16
+
+Array.prototype.square = function() {
+    return this.map(x => x * x);
+};
+
+Array.prototype.cube = function() {
+    return this.map(x => x * x * x);
+};
+
+Array.prototype.sum = function() {
+    return this.reduce((acc, val) => acc + val, 0);
+};
+
+Array.prototype.average = function() {
+    return this.length === 0 ? NaN : this.sum() / this.length;
+};
+
+Array.prototype.even = function() {
+    return this.filter(x => x % 2 === 0);
+};
+
+Array.prototype.odd = function() {
+    return this.filter(x => x % 2 !== 0);
+};
+
+//17
+
+function cache(fn) {
+    const cacheMap = new Map();
+
+    return function(...args) {
+        const key = JSON.stringify(args);
+
+        if (cacheMap.has(key)) {
+            return cacheMap.get(key);
+        }
+
+        const result = fn.apply(this, args);
+        cacheMap.set(key, result);
+        return result;
+    };
+}
+
+//18
+
+function createPhoneNumber(numbers) {
+    return `(${numbers.slice(0, 3).join('')}) ${numbers.slice(3, 6).join('')}-${numbers.slice(6).join('')}`;
+}
+
